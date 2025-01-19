9a1490ba75853ffaa944ee3f5f45245fc9c7e4d1
sohelst9 <websohel89@gmail.com>
2025-01-19 14:35:37 +0600

    meta tag and head link global handle

--- resources/js/Pages/Blogs.vue
+++ resources/js/Pages/Blogs.vue
@@ -7,6 +7,11 @@ defineProps({
 </script>
 
 <template>
+    <Head>
+        <title> | Blogs</title>
+        <meta head-key="title" name="title" content="Blog page meta title">
+        <meta head-key="description" name="description" content="Blog page meta description">
+    </Head>
     <div>
         <h2>Blogs Page</h2>
     </div>
--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -7,6 +7,11 @@ defineProps({
 </script>
 
 <template>
+    <Head>
+        <title> | Contact</title>
+        <meta head-key="title" name="title" content="Contact page meta title">
+        <meta head-key="description" name="description" content="Contact page meta description">
+    </Head>
     <div>
         <h2>Contact Page</h2>
     </div>
