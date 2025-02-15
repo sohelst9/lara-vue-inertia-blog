d5532537e964b291a1e935878400996aef33f454
sohelst9 <websohel89@gmail.com>
2025-02-15 23:49:28 +0600

    pagescontroller edit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,41 +13,11 @@ class PagesController extends Controller
     {
 
         // Fetch dynamic data for the slider
-        $sliderData = [
-            [
-                'image' => asset('assets/img/post-slide-1.jpg'),
-                'title' => 'The Best Homemade Masks for Face (keep the Pimples Away)',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-                'link' => 'single-post.html',
-            ],
-            [
-                'image' => asset('assets/img/post-slide-2.jpg'),
-                'title' => '17 Pictures of Medium Length Hair in Layers...',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-                'link' => 'single-post.html',
-            ],
-            [
-                'image' => asset('assets/img/post-slide-3.jpg'),
-                'title' => '13 Amazing Poems from Shel Silverstein...',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-                'link' => 'single-post.html',
-            ],
-            [
-                'image' => asset('assets/img/post-slide-4.jpg'),
-                'title' => '9 Half-up/half-down Hairstyles...',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-                'link' => 'single-post.html',
-            ],
-        ];
+        
+        $sliderData = Post::take(4)->get();
 
         //-- trending first post
-        $trendingPost = [
-            'blogimage' => asset('assets/img/post-landscape-1.jpg'),
-            'personimage' => asset('assets/img/person-1.jpg'),
-            'title' => '11 Work From Home Part-Time Jobs You Can Do Now',
-            'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero temporibus repudiandae, inventore pariatur numquam cumque possimus exercitationem? Nihil tempore odit ab minus eveniet praesentium, similique blanditiis molestiae ut saepe perspiciatis officia nemo, eos quae cumque. Accusamus fugiat architecto rerum animi atque eveniet, quo, praesentium dignissimos',
-            'link' => '/',
-        ];
+        $trendingPost = Post::first();
 
         $categoryBasedPosts = [
             [
